e34f0c71fe9b9b2f647d84cce6cc3f293f6b4e07
EmiRoberti77 <emiliano.roberti@accenture.com>
2024-06-15 12:13:56 +0100

    simple connection

---
+++ simple_connection/index.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+//Define the schema of the document i want to read
+const UserSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+//Define Notes schema object
+const NotesSchema = mongoose.Schema({
+  text: String,
+  title: String,
+  timeStamp: { type: Date, default: Date.now },
+});
+
+//create document model
+const User = new mongoose.model('Users', UserSchema);
+
+//create document model
+const Note = new mongoose.model('Notes', NotesSchema);
+
+//connect to database
+async function main() {
+  console.log('connecting');
+  await mongoose.connect(
+    'mongodb+srv://emi:Ferrari77Emi77!@cluster0.wvmqc9u.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0'
+  );
+  console.log('connected');
+  return 'connected to db';
+}
+
+//disconnect to database
+async function closeCon() {
+  await mongoose.disconnect();
+  console.log('connection closed');
+}
+
+//run the code
+main()
+  .then(async (success) => {
+    console.log(success);
+    //create user
+    const newUser = new User({
+      name: 'emi',
+      email: 'emi@emi2.com',
+      password: 'eee',
+    });
+    await newUser.save();
+    console.log('saved', newUser);
+
+    //create note
+    const note = new Note({
+      text: 'create program',
+      title: 'node.js programming',
+    });
+
+    await note.save();
+    console.log('saved', note);
+
+    //close db
+    await closeCon();
+  })
+  .catch((err) => console.error(err));
